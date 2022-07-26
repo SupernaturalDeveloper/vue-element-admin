8cd34f74d06bdf75883945ee89c22077f3116354
神马玩意 <2210183204@qq.com>
2022-07-26 10:04:02 +0800

    ‘'useEcharts'

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,24 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
+const HomeView = () => import("@/views/home/HomeView")
+const LoginView = () => import("@/views/login/LoginView")
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
+    path: "/",
+    redirect: "home"
+  },
+  {
+    path: "/home",
+    name: "home",
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: "/login",
+    name: "login",
+    component: LoginView
   }
 ]
 
---
+++ src/views/login/LoginView.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>这是登录页</div>
+</template>
+
+<script>
+    export default {
+        name: "LoginView",
+
+        data() {
+            return {};
+        },
+
+        mounted() {},
+
+        methods: {},
+    };
+</script>
+
+<style lang="scss" scoped>
+</style>
