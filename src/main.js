f8f130dd833d2fc182bcd27a5b9eaf5eac532d45
神马玩意 <2210183204@qq.com>
2022-07-26 09:02:29 +0800

    '添加了插件'

--- src/main.js
+++ src/main.js
@@ -2,9 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import axios from "axios"
+//ElementUI start
+import ElementUI from "element-ui"
+import "element-ui/lib/theme-chalk/index.css"
+Vue.use(ElementUI);
+// ElementUI end
 Vue.config.productionTip = false
-
+Vue.prototype.$axios = axios;
 new Vue({
   router,
   store,
